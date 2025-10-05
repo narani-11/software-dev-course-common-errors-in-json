765eb15e0a5a5fd4f51cfe0320feecfdf191c82f
narani-11 <naranitejaswini29@gmail.com>
2025-10-05 16:40:42 -0500

    Update project-task.js

--- project-task.js
+++ project-task.js
@@ -45,7 +45,7 @@ but it contains multiple syntax errors that prevent it from being parsed.
 // ============================================
  
 
-const validBookingJSON ={
+const validBookingJSON = {
   
   "hotelName": "Grand City Hotel",
   "checkInDate": "05-15-2024", 
@@ -78,13 +78,20 @@ const validBookingJSON ={
 // ✅ Requirements
 // ============================================
 
-/*
+
 - Use a JSON validator or linter (e.g., https://jsonlint.com/) to test your final version
 - Keep track of each fix:
   • What was wrong?
+  Invalid JSON!
+  Error: Parse error on line 1:
+  const validBookingJSON
+^
+Expecting 'STRING', 'NUMBER', 'NULL', 'TRUE', 'FALSE', '{', '[', got 'undefined'
   • Why is it a problem in JSON?
+   Because it is not valid in JSON , since it is a JavaScript file because const keyword is declaring a variable and assigns it to a object.
   • What did you change to fix it?
-*/
+ So I removed const validBookingJSON   then JSON is Valid! this is how I fixed .
+
 
 
 // ============================================
@@ -95,12 +102,15 @@ const validBookingJSON ={
 💬 Reflect and answer the following:
 
 1️⃣ What tools or techniques did you use to identify the errors?
-
-
+I used VS code to identify the syntax errors , missing commas, unmatched brackets ,semicolons and also braces.According to the question I also used JSONLint Validator and Formatter
+(https://jsonlint.com/) This helped after correcting the erors in code and validating JSON validator.
 2️⃣ How did you confirm that your corrected JSON file was valid?
+After removing the line of code const validBookingJSON and clicked on validate JSON and output is JSON is valid! Then I understood that commas,brackets and datatypes are correct.
 
 3️⃣ Which errors were the most difficult to spot? Why?
+So here I observed each line of code first I changed variable invalidBookingJSON to validBookingJSON.Finding missing commas,semicolons, double quotes, some wrong email formats,brackets, backticks I felt difficult to spot.when I used the help of Online JSON validator it helped me to find errors easily.
 
 4️⃣ What strategies can help you avoid these kinds of errors in the future?
    (e.g., syntax highlighting, linters, writing JSON by example)
 */
+Next time I will use syntax highlighting and also JavaScript linters to find errors before executing the code in VS code and also writing JSON code with proper spacing and validate JSON carefully. By writing the JSON code by example and hecking each line of code so that I can avoid errors in the future.
